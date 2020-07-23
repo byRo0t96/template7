1062b1497094eca16f742857362edbeee207d948
Yasser BDJ (Ro0t-96) <by.root96@gmail.com>
2020-07-23 14:45:47 +0000

    7.0.0

---
+++ content/js/js.js
@@ -0,0 +1,33 @@
+function js(filename, filetype){
+    if (filetype=="js"){
+        var fileref=document.createElement('script');
+        fileref.setAttribute("type","text/javascript");
+        fileref.setAttribute("src", filename);
+    }
+    else if (filetype=="css"){
+        var fileref=document.createElement("link");
+        fileref.setAttribute("rel", "stylesheet");
+        fileref.setAttribute("type", "text/css");
+        fileref.setAttribute("href", filename);
+    }
+	else if (filetype=="ico"){
+		var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+		link.type = 'image/x-icon';
+		link.rel = 'shortcut icon';
+		link.href = filename;
+		document.getElementsByTagName('head')[0].appendChild(link);
+    }
+	else if (filetype=="title"){
+		document.title=filename;
+    }
+    if (typeof fileref!="undefined")
+        document.getElementsByTagName("head")[0].appendChild(fileref);
+}
+
+js("content/css/css.css", "css");//original style
+js("content/css/langs.css", "css");//style langs
+js("content/css/fonts/Iceland/Iceland.css", "css");//fonts
+js("Ro0t96 | Official Website", "title");// page title
+js("content/img/Ro0t-96_v3.ico", "ico");// page title
+js("content/js/json.js", "js");// json
+js("content/css/modal.css", "css");//modal 
---
+++ content/js/json.js
@@ -0,0 +1,102 @@
+function posts(){
+	var opt = "";
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var json_obt = JSON.parse(this.responseText);
+			
+			var posts_html = document.getElementById('posts');
+			var html_code = "";
+			
+			var website="https://github.com/{user}/{repository}/";
+			var website_package="https://raw.githubusercontent.com/{user}/{repository}/master/";
+			
+			for (i = 0; i < json_obt.posts.length; i = 1) {
+				
+
+				
+				//var https=json_obt.posts[i].screenshot.substring(0,8);
+				
+				
+				var r_link = website.replace("{user}",json_obt.posts[i].user);
+				var r_link = r_link.replace("{repository}",json_obt.posts[i].repository);
+				
+				var r_package = website_package.replace("{user}",json_obt.posts[i].user);
+				var r_package = r_package.replace("{repository}",json_obt.posts[i].repository);
+				
+				if(json_obt.posts[i].screenshot.substring(0,8) == "https://" || json_obt.posts[i].screenshot.substring(0,7) == "http://"){
+					var screenshot = json_obt.posts[i].screenshot;
+				}else if(json_obt.posts[i].screenshot == ""){
+					var screenshot = "content/img/no_image.jpg";
+				}
+				else{
+					var screenshot = r_package+json_obt.posts[i].screenshot;
+				}
+				
+				
+				
+				
+				
+				
+				if(json_obt.posts[i].description.substring(0,8) == "https://" || json_obt.posts[i].description.substring(0,7) == "http://"){
+					var desc_url = json_obt.posts[i].description;
+					var desc_text = "";
+					var xmlhttp_4 = new XMLHttpRequest();
+					xmlhttp_4.open("GET", desc_url, false);
+					xmlhttp_4.send();
+					desc_text = xmlhttp_4.responseText;
+				}else if(json_obt.posts[i].description=="description.txt"){
+					var desc_url = r_package+"description.txt";
+					var desc_text = "";
+					var xmlhttp_4 = new XMLHttpRequest();
+					xmlhttp_4.open("GET", desc_url, false);
+					xmlhttp_4.send();
+					desc_text = xmlhttp_4.responseText;
+				}else if(json_obt.posts[i].description==""){
+					desc_text = "No Description";
+				}else{
+					desc_text = json_obt.posts[i].description;
+				}
+				
+				var open_modal = "'"+json_obt.posts[i].repository+"'";
+				
+				html_code+='<!-- '+json_obt.posts[i].title+' -->';
+				html_code+='<div id="repo_'+json_obt.posts[i].repository+'" class="card php">';
+				html_code+='<h2><a class="pointer" onclick="open_modal('+open_modal+')"> # '+json_obt.posts[i].title+'</a></h2>';
+				html_code+='<div class="fakeimg center"><img class="img" src="'+screenshot+'"></div>';
+				html_code+='<p>'+desc_text+'</p></div>';
+                
+				html_code+='<div id="'+json_obt.posts[i].repository+'" class="modal-window none"><div>';
+				html_code+='<a href="#repo_'+json_obt.posts[i].repository+'" title="Close" class="modal-close">Close</a>';
+				html_code+='hiiiiiiiii';
+				html_code+='</div></div>';
+				
+				
+				
+				html_code+='<!-- '+json_obt.posts[i].title+' -->';
+				
+
+
+				
+				
+				//html_code+=json_obt.posts[i].user;
+				
+			}
+            posts_html.innerHTML=html_code;
+
+	}
+	};
+	xmlhttp.open("GET", "content/json/json.json", true);
+	xmlhttp.send();
+}
+
+posts();
+
+//start
+function open_modal(hachtag){
+	/*var hash = location.hash;*/
+	var url = '#' + hachtag;
+	window.location.href = url;
+	document.getElementById(hachtag).style.display = "block";
+}
+//end
